6fa5fd54935369d480e8cdeef425b557e7964e2d
Layona-x <109739779+Layona-x@users.noreply.github.com>
2022-11-15 14:11:32 +0300

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+const path = require("path");
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      find: '@/',
+      replacement: path.resolve(__dirname, './src')
+    }
+  },
+  build: {
+    outDir: "build"
+  },
+  server: {
+    strictPort: true,
+    hmr: {
+      port: 443 // Run the websocket server on the SSL port
+    }
+  }
+})
